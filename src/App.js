3d03210e901d6e199f495f86cdf4c1dda7162313
Cass May <cass@cassm.net>
2022-03-17 18:57:32 +0000

    set up react-router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Routes} from 'react-router-dom';
 import {Frontpage} from "./Frontpage";
 import ShaderBackdrop from "./ShaderBackdrop";
 import {useState, useEffect} from "react";
@@ -20,26 +22,30 @@ function App() {
 
   return (
     <section className="App">
-      <Navigation className='navbar'/>
+      <Router>
+        <Navigation className='navbar'/>
 
-      <div className='page'>
-        <svg width='0' height='0'>
-          <defs>
-            <clipPath id='moon' clipPathUnits = 'userSpaceOnUse'>
-              <circle cx={width*0.5} cy={0} r={width*0.4}/>
-            </clipPath>
-          </defs>
-        </svg>
+        <div className='page'>
+          <svg width='0' height='0'>
+            <defs>
+              <clipPath id='moon' clipPathUnits = 'userSpaceOnUse'>
+                <circle cx={width*0.5} cy={0} r={width*0.4}/>
+              </clipPath>
+            </defs>
+          </svg>
 
-        <div className='shader-blur'>
-          <div className='shader-backdrop'>
-            <ShaderBackdrop/>
+          <div className='shader-blur'>
+            <div className='shader-backdrop'>
+              <ShaderBackdrop/>
+            </div>
           </div>
+            <div className='page-section'>
+              <Routes>
+                <Route path='/' element={<Frontpage/>}/>
+              </Routes>
+            </div>
         </div>
-        <div className='page-section'>
-          <Frontpage/>
-        </div>
-      </div>
+      </Router>
     </section>
   );
 }
