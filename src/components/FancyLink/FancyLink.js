1ac99b4fbfca5ccc5c7048c39970a38e8ce5daf6
Cass May <cass@cassm.net>
2022-04-01 16:54:21 +0100

    redefine fancylink svgs on window resize
    this prevents a bug where the effect was broken when the site was opened in narrow-screen and the window was then expanded

--- src/components/FancyLink/FancyLink.js
+++ src/components/FancyLink/FancyLink.js
@@ -12,6 +12,12 @@ const FancyLink = (props) => {
 
   const [dimensions, setDimensions] = useState({x: 0, y: 0, aspectRatio: 0})
   const [svgWidth, setSvgWidth] = useState(100);
+  const [bgSvg, setBgSvg] = useState(
+    <svg id='link-bg'
+         style={{marginTop: -dimensions.y, height: dimensions.y, width: dimensions.x}}/>);
+  const [activeSvg, setActiveSvg] = useState(
+    <svg id='link-bg-active'
+         style={{marginTop: -dimensions.y, height: dimensions.y, width: dimensions.x}}/>);
 
   useEffect(() => {
     if (ref && ref.current) {
@@ -26,63 +32,77 @@ const FancyLink = (props) => {
 
 
   useEffect(() => {
-    openTl.current = gsap.timeline({paused: true, defaults: {duration: 0.15, ease: "back.out(1.8)"}});
-    activeTl.current = gsap.timeline({paused: true});
+    if (dimensions.x !== 0 && dimensions.y !== 0) {
+      setBgSvg(
+        <svg id="link-bg" viewBox={`0 0 ${svgWidth} 100`}
+             style={{marginTop: -dimensions.y, height: dimensions.y, width: dimensions.x}}>
+          <rect id="centre-rect" x="50" y="0" height="100" width={`${svgWidth - 100}`}/>
+          <circle id="left-circle" cx={svgWidth / 2} cy="50" r="50"/>
+          <circle id="right-circle" cx={svgWidth / 2} cy="50" r="50"/>
+        </svg>);
 
-    openTl.current
-      .set(q("#link-bg"), {opacity: 0, scale: 1})
-      .set(q("#left-circle, #right-circle"), {scale: 0, transformOrigin: "50% 50%"})
-      .set(q("#centre-rect"), {scale: 0, transformOrigin: "50% 50%"})
-      .set(q("#left-circle"), {x: 0})
-      .to(q("#link-bg"), {opacity: 0.15, duration: 0.05}, "0")
-      .to(q("#left-circle, #right-circle"), {scale: 1, duration: 0.1, ease: "back.out(1.2)"}, "0")
-      .to(q("#right-circle"), {x: svgWidth/2 - 50}, "<")
-      .to(q("#left-circle"), {x: -(svgWidth/2 - 50)}, "<")
-      .to(q("#centre-rect"), {scaleX: 1}, "<")
-      .to(q("#centre-rect"), {scaleY: 1, duration: 0.1, ease: "back.out(1.6)"}, "<")
-
-    activeTl.current
-      .to(q("#link-bg-active"), {opacity: 0.25, duration: 0.1}, "0")
-      .to(q("#link-bg-active"), {scale: 1.5, opacity: 0, duration: 0.4, ease: ".out"}, ">-0.05");
-
-    const openBg = () => {
-      openTl.current.play();
+      setActiveSvg(
+        <svg id="link-bg-active" viewBox={`0 0 ${svgWidth} 100`}
+             style={{marginTop: -dimensions.y, height: dimensions.y, width: dimensions.x}}>
+          <rect id="centre-rect-active" x="50" y="0" height="100" width={`${svgWidth - 100}`}/>
+          <circle id="left-circle-active" cx="50" cy="50" r="50"/>
+          <circle id="right-circle-active" cx={svgWidth - 50} cy="50" r="50"/>
+        </svg>);
     }
+  }, [svgWidth])
 
-    const closeBg = () => {
-      openTl.current.reverse();
+  useEffect(() => {
+    if (dimensions.x !== 0 && dimensions.y !== 0) {
+      openTl.current = gsap.timeline({paused: true, defaults: {duration: 0.15, ease: "back.out(1.8)"}});
+      activeTl.current = gsap.timeline({paused: true});
+
+      openTl.current
+        .set(q("#link-bg"), {opacity: 0, scale: 1})
+        .set(q("#left-circle, #right-circle"), {scale: 0, transformOrigin: "50% 50%"})
+        .set(q("#centre-rect"), {scale: 0, transformOrigin: "50% 50%"})
+        .set(q("#left-circle"), {x: 0})
+        .to(q("#link-bg"), {opacity: 0.15, duration: 0.05}, "0")
+        .to(q("#left-circle, #right-circle"), {scale: 1, duration: 0.1, ease: "back.out(1.2)"}, "0")
+        .to(q("#right-circle"), {x: svgWidth / 2 - 50}, "<")
+        .to(q("#left-circle"), {x: -(svgWidth / 2 - 50)}, "<")
+        .to(q("#centre-rect"), {scaleX: 1}, "<")
+        .to(q("#centre-rect"), {scaleY: 1, duration: 0.1, ease: "back.out(1.6)"}, "<")
+
+      activeTl.current
+        .to(q("#link-bg-active"), {opacity: 0.25, duration: 0.1}, "0")
+        .to(q("#link-bg-active"), {scale: 1.5, opacity: 0, duration: 0.4, ease: ".out"}, ">-0.05");
+
+      const openBg = () => {
+        openTl.current.play();
+      }
+
+      const closeBg = () => {
+        openTl.current.reverse();
+      }
+
+      const clickBg = () => {
+        activeTl.current.restart();
+      }
+
+      el.current.addEventListener("mouseenter", openBg);
+      el.current.addEventListener("mouseleave", closeBg);
+      el.current.addEventListener("click", clickBg);
+
+      return () => {
+        el.current.removeEventListener("mouseenter", openBg)
+        el.current.removeEventListener("mouseleave", closeBg)
+        el.current.removeEventListener("click", clickBg);
+      }
     }
-
-    const clickBg = () => {
-      activeTl.current.restart();
-    }
-
-    el.current.addEventListener("mouseenter", openBg);
-    el.current.addEventListener("mouseleave", closeBg);
-    el.current.addEventListener("click", clickBg);
-
-    return () => {
-      el.current.removeEventListener("mouseenter", openBg)
-      el.current.removeEventListener("mouseleave", closeBg)
-      el.current.removeEventListener("click", clickBg);
-    }
-  }, [svgWidth]);
+  }, [bgSvg, activeSvg]);
 
   return (
     <div ref={el} id='link-container'>
       <NavLink id='content' className='navlink' ref={ref} to={props.to}>
         {props.content}
       </NavLink>
-      <svg id="link-bg" viewBox={`0 0 ${svgWidth} 100`} style={{marginTop: -dimensions.y, height: dimensions.y, width: dimensions.x}}>
-        <rect id="centre-rect" x="50" y="0" height="100" width={`${svgWidth-100}`}/>
-        <circle id="left-circle" cx={svgWidth/2} cy="50" r="50"/>
-        <circle id="right-circle" cx={svgWidth/2} cy="50" r="50"/>
-      </svg>
-      <svg id="link-bg-active" viewBox={`0 0 ${svgWidth} 100`} style={{marginTop: -dimensions.y, height: dimensions.y, width: dimensions.x}}>
-        <rect id="centre-rect-active" x="50" y="0" height="100" width={`${svgWidth-100}`}/>
-        <circle id="left-circle-active" cx="50" cy="50" r="50"/>
-        <circle id="right-circle-active" cx={svgWidth-50} cy="50" r="50"/>
-      </svg>
+      {bgSvg}
+      {activeSvg}
 
     </div>
   )
